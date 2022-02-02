c1499aeeb785206292428a4771aa10f6e011f762
ctdove <christian.thomas.dove@gmail.com>
2022-02-02 13:00:06 -0500

    [FIX] Took out old code

---
+++ static/public/js/connect.js
@@ -0,0 +1,49 @@
+'use strict';
+//@ts-check
+
+const initializeServer = () => {
+	const connection = new RFB();
+
+	
+}
+
+/**
+ * @function
+ * @param {String} ip
+ * @param {Number} port
+ */
+const connectToServer = (ip='127.0.0.1', port='8080') => {
+	alert(`Connecting to server at ${ip}:${port}`);
+	
+	initializeServer();
+
+	/**import RFB from './core/rfb.js';
+        let rfb;
+        let host;
+        let port;
+	function getIP(){
+            host = prompt("IP: ");
+	}
+	function getPort(){
+            port = prompt("Port: ");
+	}
+        function connectedtohost(e){
+            alert("connected");
+        }
+        
+        function connectiondropped(e){
+            alert("connection died");
+        }
+        
+        function passwordneeded(e){
+            const password = prompt("Password: ")
+            rfb.sendCredentials({password: password});
+        }
+        let url;
+        url = "wss://" + host +":" + port;
+        rfb = new RFB(document.getElementById('screen'), url, {credentials: {password: password}});
+        rfb.addEventListener("connect",  connectedtohost);
+        rfb.addEventListener("disconnect", connectiondropped);
+        rfb.addEventListener("credentialsrequired", passwordneeded);*/
+
+}
